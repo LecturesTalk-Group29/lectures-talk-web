b2be3fd899d54d270bedc05c2041095788ec0beb
Punit Halait <40286718+halait@users.noreply.github.com>
2023-10-05 02:23:24 -0400

    fixed long video error, style changes

--- app/api/start/route.ts
+++ app/api/start/route.ts
@@ -1,11 +1,13 @@
 import { NextResponse } from 'next/server';
 import ffmpeg from 'fluent-ffmpeg';
 import internal, { Readable, Writable } from 'node:stream';
+const { finished } = require('stream/promises');
 import OpenAI, { toFile } from 'openai';
 import fs from 'node:fs';
 import { Buffer } from 'node:buffer';
 
 import dbClient from '../../db';
+import { ObjectId } from 'mongodb';
 
 const video_coll = dbClient.db('lectures_talk').collection('videos');
 
@@ -19,31 +21,74 @@ export async function GET(request: Request) {
   url = atob(url);
 
   // // TODO: Create index for videoUrl?
-  // const savedVideo = await video_coll.findOne({videoUrl: url});
-  const savedVideo = JSON.parse(fs.readFileSync('./test_document.json', 'utf-8'));
+  const savedVideo = await video_coll.findOne({videoUrl: url});
+  // const savedVideo = JSON.parse(fs.readFileSync('./test_document.json', 'utf-8'));
 
   if(savedVideo) {
     return NextResponse.json(savedVideo._id);
   }
 
-  const buffer = Buffer.from(await (await fetch(url)).arrayBuffer())
-  if (!buffer) {
-    throw 'Unable to fetch video';
+  const doc = {
+    videoUrl: url,
+    status: 'pending'
   }
-  const reader = new Readable();
-  reader._read = function () { };
-  reader.push(buffer);
-  reader.push(null);
 
-  const command = ffmpeg(reader).format('mp3').pipe();
+  const id = (await video_coll.insertOne(doc)).insertedId
+  // const id = new ObjectId('651d8065b5733e28dce1cb66')
 
-  const chunks: Buffer[] = []
+  start(url, id)
 
-  command.on('data', function (chunk: Buffer) {
-    chunks.push(chunk)
-  });
+  return NextResponse.json(id);
+}
 
-  await waitForEnd(command);
+async function waitForEnd(command: Writable | internal.PassThrough) {
+  return new Promise((resolve) => {
+    command.on('end', resolve)
+  })
+}
+
+async function start(url: string, id: ObjectId) {
+  // console.log('download video...')
+
+  // const body = (await fetch(url)).body
+
+  // if (!body) {
+  //   throw 'Unable to fetch video';
+  // }
+
+  // const videoPath = `${id}.mp4`
+  
+  // const videoStream = fs.createWriteStream(videoPath);
+  // await finished(Readable.fromWeb(body as any).pipe(videoStream));
+
+  // console.log('video download complete')
+
+  // // const reader = new Readable();
+  // // reader._read = function () { };
+  // // reader.push(buffer);
+  // // reader.push(null);
+
+  // console.log('start conversion...')
+
+  // const command = ffmpeg(videoPath).noVideo().audioFrequency(22050).audioBitrate('32k').audioChannels(1).format('mp3').pipe();
+
+  // const chunks: Buffer[] = []
+
+  // command.on('data', function (chunk: Buffer) {
+  //   chunks.push(chunk)
+  // });
+
+  // command.on('error', function(err: any, stdout: any, stderr: any) {
+  //   console.log(err.message);
+  //   console.log("stdout:\n" + stdout);
+  //   console.log("stderr:\n" + stderr);
+  // })
+
+  // await waitForEnd(command);
+
+  // console.log('conversion complete')
+
+  // fs.unlink(videoPath, function() {console.log('file deleted')})
 
   // fs.writeFileSync('test.mp3', Buffer.concat(chunks))
 
@@ -51,9 +96,14 @@ export async function GET(request: Request) {
   //   file: await toFile(Buffer.concat(chunks), 'unknown.mp3'),
   //   model: 'whisper-1',
   //   response_format: 'verbose_json'
-  // })
+  // })  as any
 
-  const api_transcription = JSON.parse(fs.readFileSync('./whisper_response_example.json', 'utf-8'));
+  // console.log(api_transcription);
+
+  // fs.writeFileSync('api_transcript.txt', JSON.stringify(api_transcription))
+
+  const api_transcription = JSON.parse(fs.readFileSync('./api_transcript.json', 'utf-8'));
+
 
   const segments: {id: number, start: number, end: number, text: string}[] = [];
 
@@ -71,20 +121,16 @@ export async function GET(request: Request) {
     language: api_transcription.language,
     duration: api_transcription.duration,
     text: api_transcription.text,
-    segments: segments
+    segments: segments,
+    status: 'complete'
   }
 
-  const result = await video_coll.insertOne(doc);
-
-  // console.log(transcription);
-
-  return NextResponse.json(result.insertedId);
-}
-
-async function waitForEnd(command: Writable | internal.PassThrough) {
-  return new Promise((resolve) => {
-    command.on('end', resolve)
-  })
+  const result = await video_coll.replaceOne({_id: id}, doc);
 }
 // https://ia801801.us.archive.org/32/items/twitter-1055445425405263875/1055445425405263875.mp4
 // aHR0cHM6Ly9pYTgwMTgwMS51cy5hcmNoaXZlLm9yZy8zMi9pdGVtcy90d2l0dGVyLTEwNTU0NDU0MjU0MDUyNjM4NzUvMTA1NTQ0NTQyNTQwNTI2Mzg3NS5tcDQ=
+
+// http://hydro.ijs.si/v01b/c9/zfxdikmedp5hoksg7zv7ok6pgmikavnz.mp4
+// aHR0cDovL2h5ZHJvLmlqcy5zaS92MDFiL2M5L3pmeGRpa21lZHA1aG9rc2c3enY3b2s2cGdtaWthdm56Lm1wNA==
+
+// ffmpeg -vn -sn -dn -i test_vid.mp4 -c:a aac -ar 22050 -b:a 32k -ac 1 test_3.m4a
