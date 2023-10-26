3e7611463e0f6f4c2e2eb26fe9a762ffe22ee9d3
Punit Halait <40286718+halait@users.noreply.github.com>
2023-10-26 16:32:34 -0400

    major: added summary feature

--- app/api/create/route.ts
+++ app/api/create/route.ts
@@ -9,9 +9,12 @@ import { Buffer } from 'node:buffer';
 import dbClient from '../db';
 import { ObjectId } from 'mongodb';
 import { authenticate } from '../auth-service';
-import { LectureData } from '@/app/lecture';
+import { LectureData, LectureSegment } from '@/app/lecture';
+import { ChatCompletionMessageParam } from 'openai/resources/chat/index.mjs';
 
 const videoColl = dbClient.db('lectures_talk').collection('videos');
+
+const openai = new OpenAI();
 
 export async function POST(request: Request) {
   const user = authenticate(request)
@@ -58,48 +61,47 @@ async function waitForEnd(command: Writable | internal.PassThrough) {
 }
 
 async function start(video: LectureData) {
-  // console.log('download video...')
-  // const body = (await fetch(video.url)).body
-  // if (!body) {
-  //   // TODO: Update DB document
-  //   throw 'Unable to fetch video';
-  // }
-  // const videoPath = `${id}.mp4`
-  // const videoStream = fs.createWriteStream(videoPath);
-  // await finished(Readable.fromWeb(body as any).pipe(videoStream));
-  // console.log('video download complete')
-  // console.log('start conversion...')
-  // const command = ffmpeg(videoPath).noVideo().audioFrequency(22050).audioBitrate('32k').audioChannels(1).format('mp3').pipe();
-  // const chunks: Buffer[] = []
-  // command.on('data', function (chunk: Buffer) {
-  //   chunks.push(chunk)
-  // });
-  // command.on('error', function(err: any, stdout: any, stderr: any) {
-  //   console.log(err.message);
-  //   console.log("stdout:\n" + stdout);
-  //   console.log("stderr:\n" + stderr);
-  // })
-  // await waitForEnd(command);
-  // console.log('conversion complete')
+  console.log('lecture started, time: ' +  + Date.now())
+  const body = (await fetch(video.url)).body
+  if (!body) {
+    // TODO: Update DB document
+    throw 'Unable to fetch video';
+  }
+  const videoPath = `${video._id}.mp4`
+  const videoStream = fs.createWriteStream(videoPath);
+  await finished(Readable.fromWeb(body as any).pipe(videoStream));
+  console.log('video download complete')
+  console.log('start conversion...')
+  const command = ffmpeg(videoPath).noVideo().audioFrequency(22050).audioBitrate('32k').audioChannels(1).format('mp3').pipe();
+  const chunks: Buffer[] = []
+  command.on('data', function (chunk: Buffer) {
+    chunks.push(chunk)
+  });
+  command.on('error', function(err: any, stdout: any, stderr: any) {
+    console.log(err.message);
+    console.log("stdout:\n" + stdout);
+    console.log("stderr:\n" + stderr);
+  })
+  await waitForEnd(command);
+  console.log('conversion complete')
 
-  // fs.unlink(videoPath, function() {console.log('file deleted')})
+  fs.unlink(videoPath, function() {console.log('file deleted')})
 
-  // fs.writeFileSync('test.mp3', Buffer.concat(chunks))
+  fs.writeFileSync('test.mp3', Buffer.concat(chunks))
 
-  // const api_transcription = await new OpenAI().audio.transcriptions.create({
-  //   file: await toFile(Buffer.concat(chunks), 'unknown.mp3'),
-  //   model: 'whisper-1',
-  //   response_format: 'verbose_json'
-  // })  as any
+  const api_transcription = await openai.audio.transcriptions.create({
+    file: await toFile(Buffer.concat(chunks), 'unknown.mp3'),
+    model: 'whisper-1',
+    response_format: 'verbose_json'
+  })  as any
 
-  // console.log(api_transcription);
+  console.log(api_transcription);
 
-  // fs.writeFileSync('api_transcript.txt', JSON.stringify(api_transcription))
+  fs.writeFileSync('ph_test_api_transcript.txt', JSON.stringify(api_transcription))
 
-  const api_transcription = JSON.parse(fs.readFileSync('./api_transcript.json', 'utf-8'));
+  // const api_transcription = JSON.parse(fs.readFileSync('./api_transcript.json', 'utf-8'));
 
-
-  const segments: {id: number, start: number, end: number, text: string}[] = [];
+  const segments: LectureSegment[] = [];
 
   for(const segment of api_transcription.segments) {
     segments.push({
@@ -110,13 +112,69 @@ async function start(video: LectureData) {
     })
   }
 
+  const summarySegements = await summarize(segments)
+  console.log(summarySegements)
+
   video.language = api_transcription.language
   video.duration = api_transcription.duration
   video.text = api_transcription.text
   video.segments = segments
+  video.summarySegments = summarySegements
   video.status = 'complete'
+  
+  console.log('lecture created, time; ' + Date.now())
 
   const result = await videoColl.replaceOne({_id: video._id}, video);
+}
+
+async function summarize(segments: LectureSegment[]) {
+  const blocks: string[] = []
+  const blockLen = 64
+  for(let i = 0; i < segments.length; i += blockLen) {
+    blocks.push(
+      segments
+        .slice(i, Math.min(i + blockLen, segments.length))
+        .map(segments => segments.text)
+        .join()
+    )
+  }
+  const promises = []
+  for(let i = 0; i != blocks.length; ++i) {
+    const systemPrompt: ChatCompletionMessageParam = {
+      role: 'system',
+      content: 'The user will enter a section of a transcript, respond with a summary of the section. Use passive voice. Only respond with summary, no preamble or postamble.'
+    }
+    if(i !== 0) {
+      const lastBlock = blocks[i - 1]
+      systemPrompt.content += ' For context, this was the previous block in the transcript: ' + lastBlock.slice(lastBlock.length - 100)
+    }
+
+    const userPrompt: ChatCompletionMessageParam = {
+      role: 'user',
+      content: blocks[i]
+    }
+
+    promises.push(openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [systemPrompt, userPrompt]
+    }))
+  }
+  const responses = await Promise.all(promises)
+
+  const summarySegements: LectureSegment[] = []
+
+  for(let i = 0; i != responses.length; ++i) {
+    summarySegements.push({
+      id: i,
+      start: segments[i * blockLen].start,
+      end: segments[Math.min(segments.length, (i + 1) * blockLen) - 1].end,
+      text: responses[i].choices[0].message.content ?? ''
+    })
+  }
+
+  console.log(summarySegements)
+
+  return summarySegements
 }
 // https://ia801801.us.archive.org/32/items/twitter-1055445425405263875/1055445425405263875.mp4
 // aHR0cHM6Ly9pYTgwMTgwMS51cy5hcmNoaXZlLm9yZy8zMi9pdGVtcy90d2l0dGVyLTEwNTU0NDU0MjU0MDUyNjM4NzUvMTA1NTQ0NTQyNTQwNTI2Mzg3NS5tcDQ=
